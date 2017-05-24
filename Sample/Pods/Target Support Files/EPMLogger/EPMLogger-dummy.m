d6bb635a7a2afa10bcdf3c06fa1783a4c3fdb0ee
kagen <1149467687@qq.com>
2017-05-24 22:28:56 +0800

    update

--- Sample/Pods/Target Support Files/EPMLogger/EPMLogger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EPMLogger : NSObject
-@end
-@implementation PodsDummy_EPMLogger
-@end
--- Sample/Pods/Target Support Files/LogInWindow/LogInWindow-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LogInWindow : NSObject
-@end
-@implementation PodsDummy_LogInWindow
-@end
